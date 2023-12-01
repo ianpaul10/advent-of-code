c692b8fb2b3eed02d018c542d77dc20e2d25cb45
Ian Paul <ian@ecocart.io>
2023-12-01 09:26:14 -0800

    Part 2 of q1 2022

--- 2022/q1/main.py
+++ 2022/q1/main.py
@@ -1,9 +1,11 @@
 def run():
+    elves = []
     top_elf = 0
     current_elf = 0
     with open("inputs.txt", "r") as f:
         for line in f:
             if line == "\n":
+                elves.append(current_elf)
                 if current_elf > top_elf:
                     top_elf = current_elf
                 current_elf = 0
@@ -12,6 +14,9 @@ def run():
             current_elf += int(line)
 
     print(top_elf)
+
+    elves.sort(reverse=True)
+    print(elves[0] + elves[1] + elves[2])
 
 
 if __name__ == "__main__":
